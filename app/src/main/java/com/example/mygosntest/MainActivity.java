5dd779f037adc1059133da35fafa53df7847e213
Administrator <1774383254@qq.com>
2017-08-19 16:24:40 +0800

    最后加数组那块有疑问，数组怎么加

--- app/src/main/java/com/example/mygosntest/MainActivity.java
+++ app/src/main/java/com/example/mygosntest/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
                 jiexiJson();//解析JSON
                 break;
             case R.id.fanxing:
+                shuzu();
+                list();
+                userResult();
+                userResult2();
                 break;
         }
     }
@@ -80,5 +88,60 @@ public class MainActivity extends AppCompatActivity {
         Log.e("user:", user.age + "");
     }
 
+    //*******泛型*********//
+    public void shuzu() {
+        Gson gson = new Gson();
+        String jsonArray = "[\"Android\",\"Java\",\"PHP\"]";
+        String[] strings = gson.fromJson(jsonArray, String[].class);
+        Log.e(" String[]:", strings.toString());
+        Log.e(" String[]:", strings[0]);
+        Log.e(" String[]:", strings[1]);
+    }
 
+    public void list() {  //Gson为我们提供了TypeToken来实现对泛型的支持，所以当我们希望使用将以上的数据解析为List时需要这样写
+        Gson gson = new Gson();
+        String jsonArray = "[\"Android\",\"Java\",\"PHP\"]";
+        String[] strings = gson.fromJson(jsonArray, String[].class);
+        List<String> stringList = gson.fromJson(jsonArray, new TypeToken<List<String>>() {
+        }.getType());
+        Log.e("stringList:", stringList.toString());
+        Log.e("stringList:", stringList.size() + "");
+    }
+
+    //泛型解析对接口POJO的设计影响
+    public void userResult() {
+        String json = "{\"code\":\"0\",\"message\":\"success\",\"data\":{\"name\":\"怪盗kidou\",\"email_address\":\"123.com\",\"age\":\"24\"}}";
+        Gson gson = new Gson();
+        UserResult userResult = gson.fromJson(json, UserResult.class);
+        User user = userResult.data;
+        Log.e("user", user.name);
+
+
+        String json2 = "{\"code\":\"0\",\"message\":\"success\",\"data\":[]}";//{"code":"0","message":"success","data":[]}
+        //String jsonArray = "{"code":"0","message":"success","data":["name":"怪盗kidou","email_address":"123.com","age":"24"]}";
+        //String jsonArray = "{"code":"0","message":"success","data":["怪盗kidou","123.com","24"]}";
+        UserListResult userListResult = gson.fromJson(json2, UserListResult.class);
+        List<User> users = userListResult.data;
+        Log.e("users", users.size() + "");
+//        Log.e("users", users.get(0).name);
+
+    }
+
+    //不再重复定义Result类
+    public void userResult2() {
+        String json = "{\"code\":\"0\",\"message\":\"success\",\"data\":{\"name\":\"怪盗kidou\",\"email_address\":\"123.com\",\"age\":24}}";
+        Gson gson = new Gson();
+        Type userType = new TypeToken<Result<User>>() {
+        }.getType();
+        Result<User> userResult = gson.fromJson(json, userType);
+        User user = userResult.data;
+        Log.e("userResult2", user.name);
+
+        String json2 = "{\"code\":\"0\",\"message\":\"success\",\"data\":[]}";
+        Type userListType = new TypeToken<Result<List<User>>>() {
+        }.getType();
+        Result<List<User>> userListResult = gson.fromJson(json2, userListType);
+        List<User> users = userListResult.data;
+        Log.e("userResult2", users.size() + "");
+    }
 }
